5426ac234a0b1ba75bf865f48332ca355940e4e0
Desktop <adamlarsson92@gmail.com>
2018-03-09 07:34:37 +0100

    Refactor: rename reducer and update refs.

---
+++ src/app/reducers/insurances.ts
@@ -0,0 +1,47 @@
+import { handleActions } from 'redux-actions';
+import { RootState } from './state';
+import { InsuranceActions } from 'app/actions/insurances';
+import { InsuranceModel } from 'app/models';
+
+const initialState: InsuranceModel[] = [{
+  id: 1,
+  title: 'initial store value',
+  category: '',
+  premium: 1
+}];
+
+export const insuranceReducer = handleActions<RootState.InsurancesState, InsuranceModel>(
+{
+  [InsuranceActions.Type.ADD_INSURANCE]: (state, action) => {
+    if (action.payload) {
+      return [
+      ...state,
+      {
+        id: state.reduce((max, insurance) => Math.max(insurance.id || 1, max), 0) + 1,
+        ...action.payload,
+        category: '',
+        premium: 1
+      }
+      ];
+    }
+    else {
+      return state;
+    }
+  },
+    [InsuranceActions.Type.DELETE_INSURANCE]: (state, action) => {
+      return state.filter((insurance) => insurance.id !== (action.payload as any));
+    },
+    [InsuranceActions.Type.EDIT_INSURANCE]: (state, action) => {
+      return state.map((insurance) => {
+        if (!insurance || !action || !action.payload) {
+          return insurance;
+        } else {
+          return (insurance.id || 0) === action.payload.id
+            ? { ...insurance, text: action.payload.title }
+            : insurance;
+        }
+      });
+    },
+},
+initialState
+);
