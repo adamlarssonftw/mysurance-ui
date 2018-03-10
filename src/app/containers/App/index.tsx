c8302035e3e37168221f8670da0b64d1d3ce8714
Desktop <adamlarsson92@gmail.com>
2018-03-10 19:35:01 +0100

    Feat: Use axios & rxjs to get data and pluck the categories from the data set.

--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -3,17 +3,25 @@ import * as style from './style.css';
 import * as classNames from 'classnames';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import axios from 'axios';
+import { fromPromise } from "rxjs/observable/fromPromise";
 import { RouteComponentProps } from 'react-router';
 import { InsuranceActions } from 'app/actions/insurances';
 import { RootState } from 'app/reducers';
 import { IInsurance } from 'app/interfaces';
 import { omit } from 'app/utils';
 import { InsuranceAdder, InsuranceList } from 'app/components';
+import { pluck } from "rxjs/operators";
+import State = App.State;
 
 export namespace App {
   export interface Props extends RouteComponentProps<void> {
     insurances: IInsurance[];
     actions: InsuranceActions;
+  }
+
+  export interface State {
+    categories: string[];
   }
 }
 
@@ -25,13 +33,31 @@ export namespace App {
     actions: bindActionCreators(omit(InsuranceActions, 'Type'), dispatch)
   })
 )
-export class App extends React.Component<App.Props> {
+export class App extends React.Component<App.Props, State> {
+  private categoryURL =
+    `https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Types_of_insurance&cmtype=subcat&format=json&origin=*`;
+  private categories$ = fromPromise(axios.get(this.categoryURL));
+
   static defaultProps: Partial<App.Props> = {
     insurances: []
   };
 
   constructor(props: App.Props, context?: any) {
     super(props, context);
+    this.state = { categories: [] };
+  }
+
+  componentDidMount() {
+    this.categories$.pipe(
+      pluck('data', 'query', 'categorymembers'),
+    ).subscribe(
+      (data: any) => {
+        const categories = data
+          .map((categoryRaw: any) => categoryRaw.title.split(':')[1]);
+        this.setState({ categories: categories });
+      },
+      (err: Error) => console.log(err)
+    );
   }
 
   render() {
