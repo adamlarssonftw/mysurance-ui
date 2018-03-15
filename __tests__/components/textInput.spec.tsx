105bbc3e60ddb4c6e7f149508f676905732d7176
Desktop <adamlarsson92@gmail.com>
2018-03-16 00:08:55 +0100

    Test: add some tests for the text-input validators.

--- __tests__/components/textInput.spec.tsx
+++ __tests__/components/textInput.spec.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { TextInput } from "../../src/app/components/textInput";
 
 describe('TextInput:', () => {
-  const wrap = shallow(
+  const validators = {
+    required: (value: string) => !!value ? null : { error: 'This field is required' },
+    numeric: (value: string) => Number.parseFloat(value) ? null : { error: 'This field should only contain valid numbers' },
+  };
+
+  const wrap = mount(
     <TextInput
-      validator={() => null}
+      validator={validators.required}
       onSave={() => null}
       title="Title"
     />
@@ -14,5 +19,25 @@ describe('TextInput:', () => {
   it('should render the title passed to it', () => {
     const title = wrap.find('p').text();
     expect(title).toEqual('Title');
+  });
+
+  it('should invalidate zero-length input', () => {
+    wrap.setProps({ validator: validators.required });
+    wrap.find('input').simulate('input', {
+        target: { value: '' }
+      }
+    );
+    expect(wrap.state().valid).toEqual(false);
+    expect(wrap.state().errors.error).toEqual('This field is required');
+  });
+
+  it('should invalidate non-numeric input', () => {
+    wrap.setProps({ validator: validators.numeric });
+    wrap.find('input').simulate('input', {
+        target: { value: 'a string, not a number' }
+      }
+    );
+    expect(wrap.state().valid).toEqual(false);
+    expect(wrap.state().errors.error).toEqual('This field should only contain valid numbers');
   });
 });
