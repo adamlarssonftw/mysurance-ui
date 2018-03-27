4f50ec2acc36884cbfe4925ee1f1b572e55960fb
Desktop <adamlarsson92@gmail.com>
2018-03-27 02:31:33 +0200

    test(textInput): update textinput to reflect changes made, removing the validation tests.

--- __tests__/components/textInput.spec.tsx
+++ __tests__/components/textInput.spec.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
-import { TextInput } from "../../src/app/components/textInput";
+import { TextInput } from '../../src/app/components/textInput';
+import { IValidatedField } from 'app/interfaces';
 
 describe('TextInput:', () => {
+  const mockedInitialState: IValidatedField = {
+    value: 'mocked value',
+    valid: true,
+    touched: false,
+    errors: [],
+    validators: []
   };
+
+  const mockedSaveHandler = jest.fn();
 
   const wrap = mount(
     <TextInput
-      validator={validators.required}
-      onSave={() => null}
+      state={mockedInitialState}
+      onSave={mockedSaveHandler}
       title="Title"
     />
   );
@@ -18,23 +27,23 @@ describe('TextInput:', () => {
     expect(title).toEqual('Title');
   });
 
-  it('should invalidate zero-length input', () => {
-    wrap.setProps({ validator: validators.required });
-    wrap.find('input').simulate('input', {
-        target: { value: '' }
+  it('should bubble input to parent through handleChange()', () => {
+    wrap.find('input').simulate('change', {
+        target: { value: 'any input' }
       }
     );
-    expect(wrap.state().valid).toEqual(false);
-    expect(wrap.state().errors.error).toEqual('This field is required');
+    expect(mockedSaveHandler).toHaveBeenCalledWith('any input');
   });
 
-  it('should invalidate non-numeric input', () => {
-    wrap.setProps({ validator: validators.numeric });
-    wrap.find('input').simulate('input', {
-        target: { value: 'a string, not a number' }
-      }
-    );
-    expect(wrap.state().valid).toEqual(false);
-    expect(wrap.state().errors.error).toEqual('This field should only contain valid numbers');
+  it(`should remain in the 'normal' state if input is invalid but has not yet been touched`, () => {
+    wrap.setProps({ state: { valid: false, errors: [{ error: `something is wrong, but I can't scream yet` }] } });
+    const input = wrap.find('input');
+    expect(input.hasClass('input--invalid')).toEqual(false);
+  });
+
+  it(`should assign an 'invalid'-class if props.state.errors contains something`, () => {
+    wrap.setProps({ state: { valid: false, touched: true, errors: [{ error: 'something is wrong, I scream' }] } });
+    const input = wrap.find('input');
+    expect(input.hasClass('input--invalid')).toEqual(true);
   });
 });
